7fde77f10c441dd7f3ea5d4b794321d706690be7
Brandon Smith <bsmith8108@gmail.com>
2014-06-02 22:59:04 -0400

    the timer now shows continuously and there is a timer for the current run and for the cumulative. The coloring is still based off of the cumulative

--- rotation.js
+++ rotation.js
@@ -8,7 +8,8 @@ $(document).ready( function() {
 		var player = $("#player").val();
 		$("#player").val("");
 		var player_string = "<div class=\"player-class\" id=\""+player+"\" onclick=\"toggletime(\'"+player+"\')\">";
-		player_string = player_string +player+"<span class=\"time\">0:00</span></div>";
+		player_string = player_string +player+"<span class=\"timeCum\">Cumulative: 0:00</span>";
+		player_string = player_string +"<span class=\"timeCurrent\">Current: 0:00</span></div>";
 		playerbox.append(player_string);
 		player_list.push(player);
 	});
@@ -22,6 +23,9 @@ $(document).ready( function() {
 var playingtime = {};
 var playing = {};
 var currentWatch = {}
+var timers = {};
+var clocks = {};
+
 function startGame() {
 	var players_length = $("#players").children().length;
 	console.log(players_length)
@@ -29,32 +33,23 @@ function startGame() {
 		playingtime[player_list[i]]=0;
 		playing[player_list[i]]=false;
 		currentWatch[player_list[i]]=new Date();
+		clocks[player_list[i]]=0;
 	}
 }
 function toggletime(player) {	
 	if (playing[player]) {
 		playing[player] = false;
+		stopTimer(player);
 		var elapsed = new Date() - currentWatch[player];
 		playingtime[player] = playingtime[player] + elapsed;
 		updateColors();
 	}
 	else {
 		playing[player] = true;
+		startTimer(player);
 		currentWatch[player] = new Date();
 		updateColors();
 	}
-	var minutes = Math.floor(playingtime[player]/60000);
-	var seconds = Math.floor(playingtime[player]/1000);
-	seconds = seconds%60;
-	if (seconds.toString().length == 1) {
-		var time_string = minutes.toString()+":0"+seconds.toString();
-	}
-	else {
-		var time_string = minutes.toString()+":"+seconds.toString();
-	}
-	
-	var span = $("#"+player+" span");
-	span.text(time_string);
 }
 
 function updateColors() {
@@ -80,4 +75,57 @@ function updateColors() {
 			$("#"+player_list[i]).css("background-color",color(playingtime[player_list[i]]));
 		}
 	}
+}
+
+var offsets = {};
+var current = {};
+
+function startTimer(person) {
+	offsets[person] = Date.now();
+	current[person] = 0;
+	var id = setInterval(function() {updateTimer(person)}, 1000);
+	timers[person] = id;
+}
+
+function updateTimer(person) {
+	var now = Date.now();
+	var d  = now - offsets[person];
+	offsets[person] = now;
+	clocks[person] += d;
+	current[person] += d;
+	render(person);
+}
+
+function stopTimer(person) {
+	clearInterval(timers[person]);
+	var span = $("div#"+person).children()[1];
+	span.innerHTML = "Current: 0:00";
+}
+
+function render(person) {
+	var minutes = Math.floor(clocks[person]/60000);
+	var seconds = Math.floor(clocks[person]/1000);
+	seconds = seconds%60;
+	if (seconds.toString().length == 1) {
+		var time_string = minutes.toString()+":0"+seconds.toString();
+	}
+	else {
+		var time_string = minutes.toString()+":"+seconds.toString();
+	}
+	
+	var span = $("div#"+person).children()[0];
+	span.innerHTML = "Cumulative: " + time_string;
+	
+	var minutes = Math.floor(current[person]/60000);
+	var seconds = Math.floor(current[person]/1000);
+	seconds = seconds%60;
+	if (seconds.toString().length == 1) {
+		var time_string = minutes.toString()+":0"+seconds.toString();
+	}
+	else {
+		var time_string = minutes.toString()+":"+seconds.toString();
+	}
+	
+	var span = $("div#"+person).children()[1];
+	span.innerHTML = "Current: " + time_string;
 }
